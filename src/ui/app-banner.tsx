8bed0b0360b2fba3bbe55185400e45e3fdbcfe07
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-03-29 17:02:30 +0000

    added a link to the main page in the app banner

--- src/ui/app-banner.tsx
+++ src/ui/app-banner.tsx
@@ -1,14 +1,18 @@
+import Link from 'next/link';
+
 import { Logo } from './logo';
 
 export function AppBanner() {
   return (
-    <div className="flex flex-col items-center">
-      <Logo className="h-[160px] w-[160px]"/>
-      <h1 className="text-5xl font-extrabold tracking-tight">
-        <span className="text-[#15dec4]">Short Links </span>
-        <span className="text-[#2D3D5D]"> for </span>
-        <span className="text-[#FF2E83]">monday.com</span>
-      </h1>
-    </div>
+    <Link href="/">
+      <div className="flex flex-col items-center">
+        <Logo className="h-[160px] w-[160px]"/>
+        <h1 className="text-5xl font-extrabold tracking-tight">
+          <span className="text-[#15dec4]">Short Links </span>
+          <span className="text-[#2D3D5D]"> for </span>
+          <span className="text-[#FF2E83]">monday.com</span>
+        </h1>
+      </div>
+    </Link>
   );
 }
