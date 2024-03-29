c39c275e0c0bb63b9e5395b350412e3bd6f2ac8d
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-03-29 14:24:42 +0000

    added a button to install the app in Monday

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import Link from 'next/link';
 
 import { AppBanner, Button } from 'ui';
@@ -35,9 +36,19 @@ export default async function Home() {
           />
         </section>
 
-        <section className="flex items-center justify-center">
+        <section className="flex items-center justify-center gap-10">
+          <a
+            href="https://auth.monday.com/oauth2/authorize?client_id=514781cd0f0fc5309eb59f13577cb981&response_type=install"
+          >
+            <img
+              alt="Add to monday.com"
+              height="40"
+              className="h-[40px]"
+              src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/uploads/Tal/4b5d9548-0598-436e-a5b6-9bc5f29ee1d9_Group12441.png"
+            />
+          </a>
           <Link href="/pricing">
-            <Button variant="black">Pricing</Button>
+            <Button variant="outline-ghost">Check our Pricing</Button>
           </Link>
         </section>
       </main>
