cfe67c876a0fd55c7cbf5254f66f225f66eabbf8
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-04-01 11:40:02 +0100

    removed the notFound import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { cookies } from 'next/headers';
-import { notFound } from 'next/navigation';
 import { sql } from '@vercel/postgres';
 
 import type { ShortLink } from 'models/links';
