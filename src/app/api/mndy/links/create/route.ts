ee26a0517e2a51bc717c1ebe56ae32788fb93a0d
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-04-03 12:02:56 +0100

    changed the validation for password & expiresAt fields when creating a link

--- src/app/api/mndy/links/create/route.ts
+++ src/app/api/mndy/links/create/route.ts
@@ -10,8 +10,8 @@ import { KEBAB_CASE_REGEX } from 'utils/strings';
 const validator = z.object({
   url: z.string().min(1, { message: 'The url can not be empty' }).url({ message: 'The url is invalid' }),
   slug: z.string().min(1, { message: 'The short name can not be empty' }).regex(KEBAB_CASE_REGEX, { message: 'The short name is invalid' }),
-  password: z.string().optional(),
-  expiresAt: z.string().optional(),
+  password: z.string().nullish(),
+  expiresAt: z.string().nullish(),
 });
 
 export const POST = withAxiom(async (req: AxiomRequest) => {
