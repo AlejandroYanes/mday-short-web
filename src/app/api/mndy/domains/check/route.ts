a6f6383cae7b277823518222be3c56d1fc362ee7
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-04-21 10:31:38 +0100

    MSL-13: updated the domain on the check API route

--- src/app/api/mndy/domains/check/route.ts
+++ src/app/api/mndy/domains/check/route.ts
@@ -1,4 +1,5 @@
 import { type AxiomRequest, withAxiom } from 'next-axiom';
+import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 
 import { env } from 'env';
@@ -32,8 +33,20 @@ export const POST = withAxiom(async (req: AxiomRequest) => {
     return new Response(JSON.stringify({ status: 'invalid' }), { status: 400, headers });
   }
 
+  const client = await sql.connect();
+
   try {
     const { domain } = input.data;
+
+    const domainQuery = await client.sql<{ id: number; name: string; configured: boolean; verified: boolean }>`
+      SELECT id, name, configured, verified FROM "Domain"
+      WHERE name = ${domain} AND "workspaceId" = ${session.workspace}`;
+
+    if (domainQuery.rows.length === 0) {
+      log.error('Domain not found', { domain });
+      client.release();
+      return new Response(JSON.stringify({ status: 'not_found' }), { status: 404, headers });
+    }
 
     const [configResponse, domainResponse] = await Promise.allSettled([
       fetch(
@@ -56,15 +69,27 @@ export const POST = withAxiom(async (req: AxiomRequest) => {
           },
         }
       ),
-    ])
+    ]);
 
     if (domainResponse.status === 'rejected' || configResponse.status === 'rejected') {
-      log.error('Error fetching domain', { status: domainResponse.status });
+      log.error('Error fetching domain', { domain: domainResponse.status, config: configResponse.status });
+      client.release();
       return new Response(JSON.stringify({ status: 'error' }), { status: 500, headers });
     }
 
     const configJson = await configResponse.value.json();
     const domainJson = await domainResponse.value.json();
+
+    const storedDomain = domainQuery.rows[0]!;
+
+    if (!storedDomain.configured && !configJson.misconfigured) {
+      /**
+       * Domain was configured since last check
+       */
+      await client.sql`
+        UPDATE "Domain" SET configured = TRUE
+        WHERE id = ${storedDomain.id} AND "workspaceId" = ${session.workspace}`;
+    }
 
     /**
      * If domain is not verified, we try to verify now
@@ -80,28 +105,42 @@ export const POST = withAxiom(async (req: AxiomRequest) => {
             'Content-Type': 'application/json',
           },
         }
-      )
+      );
       verificationResponse = await verificationRes.json()
     }
 
     if (verificationResponse && verificationResponse.verified) {
       /**
-       * DomainConfig was just verified
+       * Domain was just verified
        */
-      return new Response(JSON.stringify({
-        configured: !configJson.misconfigured,
-        ...verificationResponse,
-      }), { status: 200, headers });
+      await client.sql`
+        UPDATE "Domain" SET verified = TRUE
+        WHERE id = ${storedDomain.id} AND "workspaceId" = ${session.workspace}`;
+
+      client.release();
+
+      return new Response(
+        JSON.stringify({
+          configured: !configJson.misconfigured,
+          ...verificationResponse,
+        }),
+        { status: 200, headers },
+      );
     }
 
-    return new Response(JSON.stringify({
-      configured: !configJson.misconfigured,
-      ...domainJson,
-      ...(verificationResponse ? { verificationResponse } : {}),
-    }), { status: 200, headers });
+    client.release();
+
+    return new Response(
+      JSON.stringify({
+        configured: !configJson.misconfigured,
+        ...domainJson,
+        ...(verificationResponse ? { verificationResponse } : {}),
+      }),
+      { status: 200, headers },
+    );
   } catch (error) {
-    console.log('error', error);
     log.error('Error fetching domains', { error });
+    client.release();
     return new Response(JSON.stringify({ status: 'error' }), { status: 500, headers });
   }
 });
