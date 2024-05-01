324ba62546b305f1db2abc1fd18dbe22f73cf6ff
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-05-01 22:59:13 +0100

    added Google Ads tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 import { TRPCReactProvider } from 'trpc/react';
 import { ThemeProvider } from 'ui';
+
 import 'styles/globals.css';
 
 const inter = Inter({
@@ -22,6 +24,21 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16550360967"
+          strategy="lazyOnload"
+        />
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-16550360967');
+          `}
+        </Script>
+      </head>
       <body className={`font-sans ${inter.variable}`}>
         <TRPCReactProvider>
           <ThemeProvider>
