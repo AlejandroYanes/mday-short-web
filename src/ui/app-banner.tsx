e9fae45d567bd7849db55026d0eb0f6a57473d3e
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-05-27 16:49:23 +0100

    update the banner to remove reference to monday

--- src/ui/app-banner.tsx
+++ src/ui/app-banner.tsx
@@ -7,11 +7,13 @@ export function AppBanner() {
   return (
     <Link href={env.PLATFORM_URL}>
       <div className="flex flex-col items-center">
-        <Logo className="h-[160px] w-[160px]"/>
-        <h1 className="text-5xl text-center font-extrabold tracking-tight">
-          <span className="text-[#15dec4]">Short Links </span>
-          <span className="text-[#2D3D5D]"> for </span>
-          <span className="text-[#FF2E83]">monday.com</span>
+        <div className="flex items-center">
+          <Logo className="h-[160px] w-[160px]"/>
+        </div>
+        <h1 className="text-5xl text-center font-extrabold tracking-tight mt-4">
+          <span className="text-[#15dec4]">Simple, fast </span>
+          <span className="text-[#2D3D5D] dark:text-white">and reliable </span>
+          <span className="text-[#FF2E83]">link shortener</span>
         </h1>
       </div>
     </Link>
